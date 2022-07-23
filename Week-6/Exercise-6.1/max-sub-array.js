2410a0e1daf1b512730bae5e141c22439337ca6c
sachin286 <46752637+sachin286@users.noreply.github.com>
2022-07-23 09:12:10 +0530

    first exercises

---
+++ Week-6/Exercise-6.1/max-sub-array.js
@@ -0,0 +1,22 @@
+function maxSubArraySum( a, size)  
+{  
+    let current_sum = 0;
+  let maximum_sum = 0;  
+   
+    for (let i = 0; i < size; i++)  
+    {  
+        current_sum = current_sum + a[i];  
+        if (current_sum > maximum_sum)  
+           maximum_sum = current_sum;  
+   
+        if (current_sum< 0)  
+           current_sum = 0;  
+    }  
+    return maximum_sum;  
+}
+
+let x = [1,2,-3,-9,-7];
+console.log("max sub array = " + maxSubArraySum(x,x.length));
+
+//Time complexity = 0(n)
+//Space complexity = 0(1) + 2
