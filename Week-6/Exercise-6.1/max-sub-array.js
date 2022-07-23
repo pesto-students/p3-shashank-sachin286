5798d8fb4b210a1724f436b6275e3cdbe7a39a8f
sachin286 <46752637+sachin286@users.noreply.github.com>
2022-07-23 23:20:14 +0530

    formatting with prettier

--- Week-6/Exercise-6.1/max-sub-array.js
+++ Week-6/Exercise-6.1/max-sub-array.js
@@ -1,22 +1,18 @@
-function maxSubArraySum( a, size)  
-{  
-    let current_sum = 0;
-  let maximum_sum = 0;  
-   
-    for (let i = 0; i < size; i++)  
-    {  
-        current_sum = current_sum + a[i];  
-        if (current_sum > maximum_sum)  
-           maximum_sum = current_sum;  
-   
-        if (current_sum< 0)  
-           current_sum = 0;  
-    }  
-    return maximum_sum;  
+function maxSubArraySum(a, size) {
+  let current_sum = 0;
+  let maximum_sum = 0;
+
+  for (let i = 0; i < size; i++) {
+    current_sum = current_sum + a[i];
+    if (current_sum > maximum_sum) maximum_sum = current_sum;
+
+    if (current_sum < 0) current_sum = 0;
+  }
+  return maximum_sum;
 }
 
-let x = [1,2,-3,-9,-7];
-console.log("max sub array = " + maxSubArraySum(x,x.length));
+let x = [1, 2, -3, -9, -7];
+console.log("max sub array = " + maxSubArraySum(x, x.length));
 
 //Space complexity = 0(1) + 2
 //Time complexity = 0(n)
