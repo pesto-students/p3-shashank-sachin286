37fb9f7e6e8075c8d90f269631c4768b0a8b6f62
sachin286 <46752637+sachin286@users.noreply.github.com>
2022-07-30 12:12:32 +0530

    update 2

---
+++ Week-7/Exercise-7.3/loop-list.js
@@ -0,0 +1,44 @@
+var head;
+
+class Node {
+  constructor(val) {
+    this.data = val;
+    this.next = null;
+    this.flag = 0;
+  }
+}
+
+function push(data) {
+  let temp = new Node(data);
+  temp.next = head;
+  head = temp;
+}
+
+function loopCheck() {
+  let current = head;
+  while (current != null) {
+    if (current.flag == 1) {
+      return true;
+    }
+    current.flag = 1;
+    current = current.next;
+  }
+}
+
+push(10);
+push(20);
+push(30);
+push(40);
+push(50);
+
+//creating a loop here
+head.next.next.next.next = head;
+
+if (loopCheck()) {
+  console.log("Loop is present");
+} else {
+  console.log("No Loop in the list");
+}
+
+//Time Complexity: O(N)
+//Auxiliary Space: O(1)
